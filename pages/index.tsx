9e93137d6b9bd36384788df7f909c9f6c06e0ecb
MrPee309 <linecker309@yahoo.com>
2025-09-03 18:46:35 -0400

    Add files via upload

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,23 @@
 import Navbar from '../components/Navbar';
+import PromoTicker from '../components/PromoTicker';
+import HeroSlider from '../components/HeroSlider';
 
 export default function Home() {
   return (
     <main className="container">
+      {/* Navbar */}
       <Navbar />
-      <section style={{marginTop:20}}>
+
+      {/* Promo message ticker */}
+      <PromoTicker />
+
+      {/* Hero image slider */}
+      <HeroSlider />
+
+      {/* Section prensipal */}
+      <section style={{ marginTop: 20 }}>
         <h1 className="hero-title">Check IMEI & Unlock your device</h1>
-        <p className="hero-sub">Landing verified with Pages Router.</p>
+        <p className="hero-sub">Landing verified with API-powered unlocking</p>
         <div className="row">
           <a className="button" href="/check">Start IMEI Check</a>
           <a className="button ghost" href="/unlock">Order Unlock</a>
