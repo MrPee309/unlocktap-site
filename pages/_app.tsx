68b7f2fdcfadcbcbb2442e103ca3bfe7bf5ed194
MrPee309 <linecker309@yahoo.com>
2025-09-06 16:04:15 -0400

    Add files via upload

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import type { AppProps } from 'next/app'
 import TopNav from '../components/TopNav'
 import { AuthProvider } from '../context/AuthContext'
-import '../styles/globals.css' // if you don't have this file, remove the line
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
