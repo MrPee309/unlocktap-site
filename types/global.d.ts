bfc93badfbf21408f1837da433d8646dd1205393
MrPee309 <linecker309@yahoo.com>
2025-09-06 18:53:12 -0400

    Add files via upload

---
+++ types/global.d.ts
@@ -0,0 +1,6 @@
+// types/global.d.ts
+declare module "firebase/app";
+declare module "firebase/auth";
+declare module "firebase/firestore";
+declare module "firebase/storage";
+declare module "firebase/functions";
