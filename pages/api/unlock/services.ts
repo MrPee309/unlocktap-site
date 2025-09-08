1eefc79de98178124c7568390938ccefc4e0f2ba
MrPee309 <linecker309@yahoo.com>
2025-09-08 10:03:56 -0400

    Add files via upload

--- pages/api/unlock/services.ts
+++ pages/api/unlock/services.ts
@@ -8,7 +8,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       res.setHeader("Allow", ["GET"]);
       return res.status(405).json({ success: false, error: "Method Not Allowed" });
     }
-
     const data = await dhruServiceList();
     return res.status(200).json({ success: true, data });
   } catch (err: any) {
