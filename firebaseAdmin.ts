69142acd7abcd34ce8295bc9fcc3f2e60ad298af
MrPee309 <linecker309@yahoo.com>
2025-09-14 15:48:42 -0400

    Add files via upload

---
+++ firebaseAdmin.ts
@@ -0,0 +1,15 @@
+import { getApps, getApp, initializeApp, cert } from "firebase-admin/app";
+
+let adminApp;
+if (!getApps().length) {
+  const raw = process.env.FIREBASE_SERVICE_ACCOUNT_JSON;
+  if (!raw) {
+    throw new Error("Missing FIREBASE_SERVICE_ACCOUNT_JSON env");
+  }
+  const serviceAccount = JSON.parse(raw);
+  adminApp = initializeApp({ credential: cert(serviceAccount) });
+} else {
+  adminApp = getApp();
+}
+
+export { adminApp };
