8857e0df0b69cb18a63f7b87a9a88a66b6d42770
MrPee309 <linecker309@yahoo.com>
2025-09-15 14:27:45 -0400

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: { ignoreBuildErrors: false },
-  reactStrictMode: true,
-}
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+      { protocol: 'https', hostname: 'firebasestorage.googleapis.com' },
+      { protocol: 'https', hostname: '**' }
+    ]
+  }
+};
 module.exports = nextConfig;
