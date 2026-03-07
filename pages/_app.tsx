af569df54646631635b58995345ba93e511b4505
MrPee309 <linecker309@yahoo.com>
2026-03-06 21:32:53 -0500

    Add Navbar component to MyApp layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
+import Navbar from "../components/Navbar";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  );
 }
