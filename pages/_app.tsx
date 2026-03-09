0934e025ab7b3ac7ff6851be7c365140f91555be
MrPee309 <linecker309@yahoo.com>
2026-03-09 19:44:20 -0400

    Add TopNav component to MyApp

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,11 @@
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
-
+import TopNav from "../components/TopNav";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-    
+     <TopNav />    
       <Component {...pageProps} />
     </>
   );
