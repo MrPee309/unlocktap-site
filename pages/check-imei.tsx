a3b8d06ac2ae59aae979d1e8aeb173b531c46f2d
MrPee309 <linecker309@yahoo.com>
2026-03-25 18:54:56 -0400

    Update check-imei.tsx

--- pages/check-imei.tsx
+++ pages/check-imei.tsx
@@ -2,13 +2,12 @@
 
 import dynamic from "next/dynamic";
 
-// Dynamic import, ssr false pou ImeiChecker
 const ImeiChecker = dynamic(() => import("../components/ImeiChecker"), { ssr: false });
 
 export default function CheckImeiPage() {
   return (
     <main className="p-6">
-      <h1 className="text-2xl font-bold mb-6 text-center">Check IMEI – UnlockTap</h1>
+      <h1>Check IMEI</h1>
       <ImeiChecker />
     </main>
   );
